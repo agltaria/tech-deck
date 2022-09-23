0c46868bae8e1577d50f1e790caa36e183454b25
christa-zhang-uts <69833830+agltaria@users.noreply.github.com>
2022-09-23 10:53:19 +1000

    Update single asset check

--- sceneBuilder.py
+++ sceneBuilder.py
@@ -6,19 +6,30 @@ import maya.cmds as cmds
 #def loadAssets():
     
 
-#def updateSingle():
-    #if one asset selected:
+def updateSingle():
+    objects = []
+    #checking how many objects are selected
+    for selection in cmds.ls(sl=True):
+        objects.append(selection) 
+    if len(objects) > 1:
+        if cmds.window('alert', exists = True):
+            cmds.deleteUI('alert')
+        cmds.window('alert', title = "Warning", w = 300, h = 25)
+        info = cmds.columnLayout(co = ('both', 10))
+        cmds.text("Please select only one asset.")
+        cmds.showWindow('alert')
+        raise Exception("Please select only one asset.")
+
         #update selected asset
     #else error message
 
-#def updateAll():
+def updateAll():
+    return
     #for each asset in outliner
+    
         #check if version number is biggest
         #if not, update asset
     #catch for errors
-
-
-
 
 def sceneBuilder():
     if cmds.window('sceneBuilder', exists = True):
@@ -26,5 +37,12 @@ def sceneBuilder():
 
     cmds.window('sceneBuilder', resizeToFitChildren = True, sizeable = False)
     info = cmds.columnLayout(co = ('both', 10))
+
+    updateButtons = cmds.rowColumnLayout(parent = info, numberOfRows = 1)
+    cmds.button(label = 'Update asset', command = 'updateSingle()')
+    cmds.separator(w = 10, st = 'none')
+    cmds.button(label = 'Update all')
+    cmds.showWindow('sceneBuilder')
+
 
 sceneBuilder()
