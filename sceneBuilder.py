a750085396e6a1f64ba6e87207169524c1840dd2
christa-zhang-uts <69833830+agltaria@users.noreply.github.com>
2022-11-04 08:35:13 +1100

    Update sceneBuilder.py

--- sceneBuilder.py
+++ sceneBuilder.py
@@ -1,11 +1,142 @@
+from calendar import c
 import os
 import os.path
+from turtle import pu
 import maya.cmds as cmds
+from functools import partial
+
+
+global shotSelected #holds current selected shot
+publicFolderDirectory = r"C:\Users\lilly\Documents\maya\projects\Assessment2_GroupX\scenes\publish"
+currentProjectDirectory = cmds.workspace(q = True, dir = True)
+
+global animationShotFolder
+global layoutShotFolder
+global lightingShotFolder
+
+def getPublishDirectory():
+    publishDirectory = currentProjectDirectory + "scenes/publish"
+    return publishDirectory
+
+def returnSequences():
+    return getPublishDirectory() + "/sequence"
+
+def returnAssets():
+    return getPublishDirectory() + "/assets"
+
+def selectSequenceFolder():
+    global shotSelected
+    global animationShotFolder
+    global layoutShotFolder
+    global lightingShotFolder
+
+    shotSelected = cmds.fileDialog2(fileMode = 3, dir = returnSequences())
+    shotSelect = cmds.textField('shotSelect', edit = True, text = str(os.path.basename(shotSelected[0]))) #updates textfield
+    animationShotFolder = shotSelected[0] + '/animation/source/'
+    layoutShotFolder = shotSelected[0] + '/layout/source/'
+    lightingShotFolder = shotSelected[0] + '/lighting/source/' 
+    return shotSelected[0]
+
+def loadShot(folder, *args):
+    for file in os.listdir():
+        if file.endswith(".mb"):
+            print(os.path.join(folder, file))
+    return
+
+def selectAssetFolder():
+    assetSelected = cmds.fileDialog2(fileMode = 3, dir = returnAssets())
+
+def loadLayout():
+    global layoutShotFolder
+    global shotSelected
+
+    files = []
+    files2 = []
+    alembic = ""
+    latestFile = ""
+    for file in os.listdir(layoutShotFolder):
+        if file.endswith(".mb"):
+            files.append(file)
+            latestFile = file
+    for i in files:
+        if(isLatest(i, latestFile)):
+            latestFile = i
+
+    alembicPath = shotSelected[0] + "/layout/cache/alembic/"
+    for file in os.listdir(alembicPath):
+        if file.endswith(".abc"):
+            files2.append(file)
+            alembic = file
+    for i in files2:
+        if(isLatest(i, alembic)):
+            alembic = i
+    cmds.file(alembicPath + alembic, reference = True)
+    path = layoutShotFolder + latestFile
+    cmds.file(path, reference = True)
+
+def loadAnimation():
+    global animationShotFolder
+    global shotSelected
+    files = []
+    files2 = []
+    latestFile = ""
+    alembic = ""
+    for file in os.listdir(animationShotFolder):
+        if file.endswith(".mb"):
+            files.append(file)
+            latestFile = file
+    for i in files:
+        if(isLatest(i, latestFile)):
+            latestFile = i
+
+    path = animationShotFolder + latestFile
+    alembicPath = shotSelected[0] + "/animation/cache/alembic/"
+    for file in os.listdir(alembicPath):
+        if file.endswith(".abc"):
+            files2.append(file)
+            alembic = file
+    for i in files2:
+        if(isLatest(i, alembic)):
+            alembic = i
+    cmds.file(alembicPath + alembic, reference = True)
+    cmds.file(path, reference = True)
+
+def loadLighting():
+    global lightingShotFolder
+    global shotSelected
+    files = []
+    files2 = []
+    latestFile = ""
+    alembic = ""
+    for file in os.listdir(lightingShotFolder):
+        if file.endswith(".mb"):
+            files.append(file)
+            latestFile = file
+    for i in files:
+        if(isLatest(i, latestFile)):
+            latestFile = i
+
+    path = lightingShotFolder + latestFile
+    alembicPath = shotSelected[0] + "/light/cache"
+    for file in os.listdir(alembicPath):
+        if file.endswith(".abc"):
+            files2.append(file)
+            alembic = file
+    for i in files2:
+        if(isLatest(i, alembic)):
+            alembic = i
+    cmds.file(path, reference = True)
+
 
 def loadAsset(newRefPath, currentRefNode):
     cmds.file(newRefPath, loadReference = currentRefNode)
     return
 
+def isLatest(currentFile, directoryFile): #compare current file version with files in directory
+    if readFileVersion(currentFile) >= readFileVersion(directoryFile):
+        return True
+    else:
+        return False
 
 #read version number
 def readFileVersion(fileName):
@@ -26,13 +157,6 @@ def getRelevantFiles(filesInDir, selectedFile): #gets all file versions and filt
         if fileName in file:
             relevantFiles.append(file)
     return relevantFiles
-
-
-def isLatest(currentFile, directoryFile): #compare current file version with files in directory
-    if readFileVersion(currentFile) >= readFileVersion(directoryFile):
-        return True
-    else:
-        return False
 
 def returnLatest(currentFile): #returns name of most updated file
         filePath = cmds.referenceQuery(currentFile, f = True)
@@ -149,19 +273,58 @@ def sceneBuilder():
     cmds.window('sceneBuilder', resizeToFitChildren = True, sizeable = False)
     info = cmds.columnLayout(co = ('both', 10))
     cmds.text("SCENE BUILDER")
+    cmds.text("Please ensure your project is set to the correct directory.")
+
+    cmds.separator(h = 10)
+
+    cmds.text("Select shot to load")
+    cmds.rowColumnLayout(parent = info, numberOfRows = 1)
+    shotSelect = cmds.textField('shotSelect', width = 200, editable = False)
+    cmds.button(label = 'Select', command = 'selectSequenceFolder()')
+    cmds.separator(h = 20)
+
+    loadButtons = cmds.rowColumnLayout(parent = info, numberOfRows = 1)
+    cmds.button(label = 'Load layout', command = 'loadLayout()')
+    cmds.separator(w = 10, st = 'none')
+    cmds.button(label = 'Load animation', command = 'loadAnimation()')
+    cmds.separator(w = 10, st = 'none')
+    cmds.button(label = 'Load lighting', command = 'loadLighting()')
+
+
+    cmds.separator(parent = info, h = 10)
+    cmds.separator(parent = info, w = 100)
+    cmds.separator(parent = info, h = 10)
+
+    cmds.text(parent = info, label = "Load an asset")
+    cmds.rowColumnLayout(parent = info, numberOfRows = 1)
+    setSelect = cmds.textField('setSelect', width = 200, editable = False)
+    cmds.button(label = 'Select', command = 'selectSequenceFolder()')
+    cmds.separator(h = 20)
+
     updateButtons = cmds.rowColumnLayout(parent = info, numberOfRows = 1)
     cmds.button(label = 'Check for updates', command = 'checkAllForUpdates()')
     cmds.separator(w = 10, st = 'none')
     cmds.button(label = 'Update asset', command = 'updateSingle()')
     cmds.separator(w = 10, st = 'none')
     versions = cmds.optionMenu()
-
+    
     cmds.separator(w = 10, st = 'none')
     cmds.button(label = 'Update all', command = 'updateAll()')
+
+
+
+   
+
     cmds.showWindow('sceneBuilder')
 
-sceneBuilder()
 
+
+    
+
+
+
+sceneBuilder()
+print(returnSequences())
 """
 referenceList = cmds.ls(rf = True)
 
@@ -172,4 +335,6 @@ for reference in referenceList:
     indexDot = fileNameToSplit.index('.')
     fileName = fileNameToSplit[:indexDot]
     print(fileName)
+
+
 """
